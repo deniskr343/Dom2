c87e6b51d9f38a2012fdfcca394a4eb817c83ca8
olha <olya.b.d.bd@gmail.com>
2022-08-24 21:26:37 +0300

    Olya

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import turtle
 
 mainTurtle = turtle.Turtle()
-
-
-
+turtle.penup()
+turtle.goto(-150,-150)
+turtle.pendown()
+turtle.pencolor("yellow")
+turtle.forward(300)
+turtle.left(100)
+turtle.forward(200)
+turtle.left(80)
+turtle.forward(230)
+turtle.left(80)
+turtle.forward(200)
 mainTurtle.hideturtle()
+
+
 turtle.exitonclick()
