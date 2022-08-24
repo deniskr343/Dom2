a25e44eb4c9689a32f23047978ebe21687047c6b
Serj <sergeyroman26@gmail.com>
2022-08-24 19:42:51 +0100

    Fixed Danil

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import turtle
 
 mainTurtle = turtle.Turtle()
+
 def danik(x,y):
     mainTurtle.penup()
     mainTurtle.goto(x,y)
     mainTurtle.pendown()
     mainTurtle.fillcolor('yellow')
     mainTurtle.begin_fill()
+    #Тут можно оптимизировать циклом for
     mainTurtle.forward(90)
     mainTurtle.right(90)
     mainTurtle.forward(90)
@@ -15,7 +17,7 @@ def danik(x,y):
     mainTurtle.right(90)
     mainTurtle.forward(90)
     mainTurtle.end_fill()
-
+danik(100,100)
 
 
 mainTurtle.hideturtle()
