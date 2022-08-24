6ee950285b9823cd12653041cb682f80ddc3989c
Pasha <justuser166@gmail.com>
2022-08-24 21:13:44 +0300

    pashaaaaaa

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import turtle
 
 mainTurtle = turtle.Turtle()
-
+def circlePasha(x,y,circle,color):
+    mainTurtle.penup()
+    mainTurtle.goto(x,y)
+    mainTurtle.pendown()
+    mainTurtle.begin_fill()
+    mainTurtle.circle(circle)
+    mainTurtle.fillcolor(color)
+    mainTurtle.end_fill()
 
 
 mainTurtle.hideturtle()
