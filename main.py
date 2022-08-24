2d0c867a53b2c7a862ed58d90d8a8d0ba906d0bb
Serj <sergeyroman26@gmail.com>
2022-08-24 19:45:13 +0100

    Fixed Pasha

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import turtle
 
 mainTurtle = turtle.Turtle()
-def circlePasha(x,y,circle,color):
+def circlePasha(x,y):
     mainTurtle.penup()
     mainTurtle.goto(x,y)
     mainTurtle.pendown()
     mainTurtle.begin_fill()
-    mainTurtle.circle(circle)
-    mainTurtle.fillcolor(color)
+    mainTurtle.circle(50)
+    mainTurtle.fillcolor('red')
     mainTurtle.end_fill()
-
+circlePasha(100,100)
 
 mainTurtle.hideturtle()
 turtle.exitonclick()
