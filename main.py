c579a0c4080a806ff93f77a40baed03ed1a4798b
Danik <Dansus7436@gmail.com>
2022-08-24 19:24:59 +0100

    Danil

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import turtle
 
 mainTurtle = turtle.Turtle()
+def danik(x,y):
+    mainTurtle.penup()
+    mainTurtle.goto(20,20)
+    mainTurtle.pendown()
+    mainTurtle.fillcolor('yellow')
+    mainTurtle.begin_fill()
+    mainTurtle.forward(90)
+    mainTurtle.right(90)
+    mainTurtle.forward(90)
+    mainTurtle.right(90)
+    mainTurtle.forward(90)
+    mainTurtle.right(90)
+    mainTurtle.forward(90)
+    mainTurtle.end_fill()
 
 
 
