443cd4be916858342cdf64056fcb52eefc5b8b18
Geeru4 <geeru4@icloud.com>
2022-08-25 12:24:42 +0200

    "German"

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ import turtle
 
 mainTurtle = turtle.Turtle()
 
-
+mainTurtle.pensize(6)
+#квадрат
+mainTurtle.color("green")
+mainTurtle.pendown()
+mainTurtle.right(45)
+mainTurtle.forward(150)
+mainTurtle.left(90)
+mainTurtle.forward(150)
+mainTurtle.left(90)
+mainTurtle.forward(150)
+mainTurtle.left(90)
+mainTurtle.forward(150)
+mainTurtle.penup()
 
 mainTurtle.hideturtle()
 turtle.exitonclick()
