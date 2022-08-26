3701e024b6ac930bd0ea2cff78293ca776f59163
denis <deniskr290706@gmail.com>
2022-08-26 19:48:08 +0300

    denisk

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import turtle
 
 mainTurtle = turtle.Turtle()
+turtle.penup()
+turtle.goto(45,80)
+turtle.pendown()
+turtle.color("purple")
 
-
-
+turtle.forward(100)
+turtle.left(70)
+turtle.forward(100)
+turtle.left(110)
+turtle.forward(100)
+turtle.left(70)
+turtle.forward(100)
 mainTurtle.hideturtle()
 turtle.exitonclick()
